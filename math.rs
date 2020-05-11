59d6a382b8084e330ce865300d121b547e86eb38
Teo Samarzija <teo.samarzija@gmail.com>
2020-05-11 16:06:58 +0200

    Refactored "math.rs".

--- math.rs
+++ math.rs
@@ -1,91 +1,77 @@
-const PRECISION: f64=2048.; //Balance between speed and precision here.
+const PRECISION: f64=512.; //Balance between speed and precision here.
 
 fn ln(x:f64) -> f64 {
-    let mut sum=0 as f64;
+    let mut sum=0.;
     let epsilon=(x-1.)/(5.*PRECISION);
-    let mut i=1 as f64;
+    let mut i=1.;
     while (epsilon>0. && i<x) || (epsilon<0. && i>x) {
         sum+=epsilon/i;
         i+=epsilon;
     }
-    return sum;
+    sum
 }
 
 fn exp(x:f64) -> f64 {
-    let mut i=0 as f64;
-    let mut y=1 as f64;
+    let mut i=0.;
+    let mut y=1.;
     let epsilon=x/PRECISION;
     while (epsilon>0. && i<x) || (epsilon<0. && i>x) {
         y+=epsilon*y;
         i+=epsilon;
     }
-    return y;
+    y
 }
 
 fn arctan(x:f64) -> f64 {
-    let mut sum=0 as f64;
+    let mut sum=0.;
     let epsilon=x/PRECISION;
-    let mut i=0 as f64;
+    let mut i=0.;
     while i<x {
         sum+=epsilon/(1.+i*i);
         i+=epsilon;
     }
-    return sum*(180./pi());
+    sum*(180./pi())
 }
 
-fn tan(x:f64) -> f64 {
-    let radians=x/(180./pi());
-    let mut tmpsin=0 as f64;
-    let mut tmpcos=1 as f64;
-    let epsilon=x/(5.*PRECISION);
-    let mut i=0 as f64;
+fn tan(degrees:f64) -> f64 {
+    sin(degrees)/cos(degrees)
+}
+
+fn sin(degrees:f64) -> f64 {
+    if degrees<0. {
+        return -sin(-degrees);
+    }
+    if degrees>90. {
+        return cos(degrees-90.);
+    }
+    let radians=degrees/(180./pi());
+    let mut tmpsin=0.;
+    let mut tmpcos=1.;
+    let epsilon=radians/PRECISION;
+    let mut i=0.;
     while (epsilon>0. && i<radians) || (epsilon<0. && i>radians) {
         tmpsin+=epsilon*tmpcos;
         tmpcos-=epsilon*tmpsin;
         i+=epsilon;
     }
-    return tmpsin/tmpcos;
-}
-
-fn sin(x:f64) -> f64 {
-    let radians=x/(180./pi());
-    let mut tmpsin=0 as f64;
-    let mut tmpcos=1 as f64;
-    let epsilon=x/(5.*PRECISION);
-    let mut i=0 as f64;
-    while (epsilon>0. && i<radians) || (epsilon<0. && i>radians) {
-        tmpsin+=epsilon*tmpcos;
-        tmpcos-=epsilon*tmpsin;
-        i+=epsilon;
-    }
-    return tmpsin;
+    tmpsin
 }
 
 fn arcsin(x:f64) -> f64 {
-    return arctan(x/sqrt(1.-x*x));
+    arctan(x/sqrt(1.-x*x))
 }
 
 fn arccos(x:f64) -> f64 {
-    return 90.-arcsin(x);
+    90.-arcsin(x)
 }
 
-fn cos(x:f64) -> f64 {
-    let radians=x/(180./pi());
-    let mut tmpsin=0 as f64;
-    let mut tmpcos=1 as f64;
-    let epsilon=x/(5.*PRECISION);
-    let mut i=0 as f64;
-    while (epsilon>0. && i<radians) || (epsilon<0. && i>radians) {
-        tmpsin+=epsilon*tmpcos;
-        tmpcos-=epsilon*tmpsin;
-        i+=epsilon;
-    }
-    return tmpcos;
+fn cos(degrees:f64) -> f64 {
+    sin(90.-degrees)
 }
 
 fn sqrt(x:f64) -> f64 {
-    let mut max=1000 as f64;
-    let mut min=0 as f64;
+    let mut max=1000.;
+    let mut min=0.;
     let mut i=(min+max)/2.;
     while (max-min)>1./PRECISION {
         if i*i>x {
@@ -96,18 +82,18 @@ fn sqrt(x:f64) -> f64 {
         }
         i=(max+min)/2.;
     }
-    return i;
+    i
 }
 
 fn pi() -> f64 {
-    let mut sum=0 as f64;
-    let mut i=-1 as f64;
+    let mut sum=0.;
+    let mut i=-1.;
     let epsilon=1./PRECISION;
     while i<1. {
         sum+=epsilon/(1.+i*i);
         i+=epsilon;
     }
-    return 2.*sum;
+    2.*sum
 }
 
 fn main() {
@@ -116,11 +102,37 @@ fn main() {
         print!("{:.4}\t",i);
         print!("{:.4}\t",sin(i as f64));
         print!("{:.4}\t",cos(i as f64));
-        if tan(i as f64)<100. && tan(i as f64)> -10. {print!("{:.4}\t",tan(i as f64));} else if tan(i as f64)< -10. {print!("{:.3}\t",tan(i as f64));} else {print!("inf\t");}
-        if ln(i as f64)> -100. {print!("{:.4}\t",ln(i as f64));} else {print!("-inf\t");}
+        if tan(i as f64)<100. && tan(i as f64)> -10. {
+            print!("{:.4}\t",tan(i as f64));
+        }
+        else if tan(i as f64)< -10. {
+            print!("{:.3}\t",tan(i as f64));
+        }
+        else {
+            print!("inf\t");
+        }
+        if ln(i as f64)> -100. {
+            print!("{:.4}\t",ln(i as f64));
+        }
+        else {
+            print!("-inf\t");
+        }
         print!("{:.4}\t",sqrt(i as f64));
-        if arcsin((i as f64)/100.)<90. {print!("{:.4}\t",arcsin((i as f64)/100.));} else {print!("90\t");}
-        if arccos((i as f64)/100.)<90. && arccos((i as f64)/100.)>0. {print!("{:.4}\t",arccos((i as f64)/100.));} else if arccos((i as f64)/100.)>0. {print!("90\t");} else {print!("0\t");}
+        if arcsin((i as f64)/100.)<90. {
+            print!("{:.4}\t",arcsin((i as f64)/100.));
+        }
+        else {
+            print!("90\t");
+        }
+        if arccos((i as f64)/100.)<90. && arccos((i as f64)/100.)>0. {
+            print!("{:.4}\t",arccos((i as f64)/100.));
+        }
+        else if arccos((i as f64)/100.)>0. {
+            print!("90\t");
+        }
+        else {
+            print!("0\t");
+        }
         print!("{:.4}\n",arctan((i as f64)/100.));
     }
     println!("pi={:.4}",pi());
